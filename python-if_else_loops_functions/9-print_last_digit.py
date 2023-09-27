4b6a2948d1db8e91661fe0e1c378475599c4f971
daggzu <diegog8603@gmail.com>
2023-09-27 17:49:46 -0400

    oki

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if isinstance(number, int):
+        number = str(number)
+    number = int(str(number)[-1])
+    print(number, end="")
+    return number
