434465eccd20113599e244d8644b68207b5a59a1
daggzu <diegog8603@gmail.com>
2023-09-27 12:09:13 -0400

    jhonathan

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numero in range(0, 100):
     if numero < 99:
-        print("{}".format(numero), end=", ")
+        print("{:02}".format(numero), end=", ")
     else:
         print("{}".format(numero))
