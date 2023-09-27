2094664588b158a9105c399b51474bb25bfe302d
daggzu <diegog8603@gmail.com>
2023-09-27 10:08:27 -0400

    jfjf

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):  
+for letter in range(97, 123):
     if chr(letter) not in ['e', 'q']:
-        print(chr(letter), end=' ')
+        print(f"{chr(letter)}", end='')
