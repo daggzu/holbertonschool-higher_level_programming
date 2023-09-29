95d753e0d31ad016fa251e116dd77d3786b14541
daggzu <diegog8603@gmail.com>
2023-09-29 12:57:38 -0400

    kj

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,6 @@ def uppercase(str):
 	for upper in str:
 		if upper >= chr(97) and upper <= chr(122):
 			upper = chr(ord(upper) - 32)
-		print("{}".format(upper), end="")
-	print("")
+		print("{}".format(upper), end="")	
+print("")
+  
