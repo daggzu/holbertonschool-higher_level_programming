45499a8e26466e455d60f3b18ede43fde3f6a83a
daggzu <diegog8603@gmail.com>
2023-09-29 22:33:15 -0400

    d

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-# This is a shebang line, specifying the path to the Python interpreter
-# that should be used to execute the script.
-
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
 
+add = 0
+result = 0
+argument = len(sys.argv) - 1
 
-if args == 1:
-    
-    print("0")
-   
-else:
-    sum = 0
- 
-    for i in range(1, args):
-    
-
-        sum += int(sys.argv[i])
-
-    print("{}".format(sum))
+for i in range(argument):
+    add = add + 1
+    result = int(result) + int(sys.argv[add])
+print("{}".format(result))
