3402ca6d10a5035cc2de96487a5f5c5e244ef657
daggzu <diegog8603@gmail.com>
2023-10-13 16:10:58 -0400

    ok

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+class Square:
+    """Definition of square attribute"""
+
+    def __init__(self, size=0):
+        """Initialize"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return square area"""
+        square_area = self.__size ** 2
+        return square_area
