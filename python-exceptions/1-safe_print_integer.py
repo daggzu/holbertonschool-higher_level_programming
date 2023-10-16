e3afcada3d961fd583b6a7d13e883a0bd0d87406
daggzu <diegog8603@gmail.com>
2023-10-16 19:59:19 -0400

    h

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-        print()
-        return x
-    except IndexError:
-        print()
-        return i
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
+
