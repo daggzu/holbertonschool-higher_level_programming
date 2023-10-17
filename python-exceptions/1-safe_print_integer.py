47b9e32494bf566f06100223b14deda8f1223fd0
daggzu <diegog8603@gmail.com>
2023-10-16 20:03:07 -0400

    f

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
-
