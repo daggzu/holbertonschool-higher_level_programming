549d578acfa35ffdde64fb2b658efb6d75728181
daggzu <diegog8603@gmail.com>
2023-11-24 18:32:29 -0400

    no clue

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,12 @@
 #!/usr/bin/python3
-"""Module to list all states from database hbtn_0e_0_usa"""
+""" Lists all states from the database hbtn_0e_0_usa. """
 
-
-import MySQLdb
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    """Function that lists all states from database hbtn_0e_0_usa"""
-
-    # Connecting to a MySQL database.
-    cnx = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        charset="utf8",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3])
-
-    # Making cursor obj for execution.
-    cur = cnx.cursor()
-
-    # Executing query.
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Obtaining query results.
-    query_rows = cur.fetchall()
-
-    # Printing results.
-    for row in query_rows:
-        print(row)
-
-    # Close cursor.
-    cur.close()
-
-    # Close connection to database.
-    cnx.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
