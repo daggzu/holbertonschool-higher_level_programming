869b461679271f57507a8b724bbbb6078d1f4a9b
daggzu <diegog8603@gmail.com>
2023-11-24 18:28:01 -0400

    idk

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ import sys
 if __name__ == "__main__":
     """Function that lists all states from database hbtn_0e_0_usa"""
 
+    # Connecting to a MySQL database.
     cnx = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -16,15 +17,21 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         database=sys.argv[3])
 
+    # Making cursor obj for execution.
     cur = cnx.cursor()
 
+    # Executing query.
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
+    # Obtaining query results.
     query_rows = cur.fetchall()
 
+    # Printing results.
     for row in query_rows:
         print(row)
 
+    # Close cursor.
     cur.close()
 
+    # Close connection to database.
     cnx.close()
