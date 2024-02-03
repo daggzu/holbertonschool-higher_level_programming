117cd826903642f68423334555463cf5f491d20f
daggzu <diegog8603@gmail.com>
2024-02-02 21:30:09 -0400

    ths commit does 4 and  5 with documentation

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,14 @@
+let add_item = document.getElementById('add_item');
+add_item.addEventListener('click', function() {
+    // Create a new element 'li'.
+    let li = document.createElement('li');
+
+    // Add the text 'Item' to the new element 'li'.
+    li.textContent = 'Item';
+
+    // Select the html element 'my_list'.
+    let my_list = document.querySelector('.my_list');
+
+    // Add the new element 'li' to the html element 'my_list'.
+    my_list.appendChild(li);
+});
